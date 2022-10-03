82dfdccf3992e128810633cd500ec78f1265b716
Vermium Sifell <vermium@zyner.org>
2022-10-03 12:59:01 +0200

    versioned documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,86 +1,110 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Controlpanel.gg',
-  tagline: 'ControlPanel\'s Dashboard is a free and open-source management panel for Pterodactyl with credit based billing and lots of customization',
-  url: 'https://controlpanel.gg',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/controlpanel.ico',
-  organizationName: 'ControlPanel-gg', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  title: "Controlpanel.gg",
+  tagline:
+    "ControlPanel's Dashboard is a free and open-source management panel for Pterodactyl with credit based billing and lots of customization",
+  url: "https://controlpanel.gg",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/controlpanel.ico",
+  organizationName: "ControlPanel-gg", // Usually your GitHub org/user name.
+  projectName: "docs", // Usually your repo name.
   trailingSlash: false,
   themeConfig: {
     navbar: {
-      title: 'Controlpanel (CPGG)',
+      title: "Controlpanel (CPGG)",
       logo: {
-        alt: 'Controlpanel logo',
-        src: 'img/controlpanel.png',
+        alt: "Controlpanel logo",
+        src: "img/controlpanel.png",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Documentation',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Documentation",
         },
-        {
-          href: "https://demo.controlpanel.gg",
-          label: "Demo",
-          position: "left"
-        },
+
         {
           href: "https://market.controlpanel.gg",
           label: "Extention Hub",
-          position: "left"
+          position: "left",
         },
         {
-          href: 'https://documenter.getpostman.com/view/9044962/TzY69ub2#02b8da43-ab01-487d-b2f5-5f8699b509cd',
-          label: 'API',
-          position: 'right',
+          type: "docsVersionDropdown",
+          position: "right",
+          dropdownActiveClassDisabled: true,
         },
         {
-          href: 'https://discord.gg/4Y6HjD2uyU',
-          label: 'Discord',
-          position: 'right',
+          label: "Utilities",
+          position: "right",
+          items: [
+            {
+              href: "https://documenter.getpostman.com/view/9044962/TzY69ub2#02b8da43-ab01-487d-b2f5-5f8699b509cd",
+              label: "API",
+              position: "right",
+            },
+            {
+              href: "https://demo.controlpanel.gg",
+              label: "Demo",
+              position: "right",
+            },
+            {
+              href: "https://trello.controlpanel.gg/b/KGMuFqkqe5QabLt76/controlpanel",
+              label: "Development-Roadmap",
+              position: "right",
+            },
+          ],
         },
         {
-          href: 'https://github.com/ControlPanel-gg/dashboard',
-          label: 'GitHub',
-          position: 'right',
+          label: "Social",
+          position: "right",
+          items: [
+            {
+              href: "https://discord.gg/4Y6HjD2uyU",
+              label: "Discord",
+              position: "right",
+            },
+            {
+              href: "https://github.com/ControlPanel-gg/dashboard",
+              label: "GitHub",
+              position: "right",
+            },
+          ],
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Documentation',
-              to: '/docs/intro',
+              label: "Documentation",
+              to: "/docs/intro",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discord',
-              href: 'https://discord.gg/4Y6HjD2uyU',
+              label: "Discord",
+              href: "https://discord.gg/4Y6HjD2uyU",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'GitHub',
-              href: 'https://github.com/ControlPanel-gg/dashboard',
+              label: "GitHub",
+              href: "https://github.com/ControlPanel-gg/dashboard",
             },
           ],
         },
@@ -94,16 +118,15 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          lastVersion: "0.8",
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/ControlPanel-gg/docs/tree/main/',
+          editUrl: "https://github.com/ControlPanel-gg/docs/tree/main/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
